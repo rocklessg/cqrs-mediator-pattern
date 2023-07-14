53c926b430fa64b021174de24dec8b027f56b863
Abdulhafiz <adanmaigoro@fsdhgroup.com>
2023-07-14 15:46:56 +0100

    Implement database context and repository

---
+++ CqrsMediatRPattern.API/Repositories/Interfaces/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using CqrsMediatRPattern.API.Models;
+
+namespace CqrsMediatRPattern.API.Repositories.Interfaces
+{
+    public interface IStudentRepository
+    {
+        Task<List<Student>> GetStudentListAsync();
+        Task<Student> GetStudentByIdAsync(int Id);
+        Task<Student> AddStudentAsync(Student studentDetails);
+        Task<int> UpdateStudentAsync(Student studentDetails);
+        Task<int> DeleteStudentAsync(int Id);
+    }
+}
