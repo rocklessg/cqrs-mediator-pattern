d8d6cdb74878650588b70074e4607444d8aca222
Abdulhafiz <adanmaigoro@fsdhgroup.com>
2023-07-14 17:46:56 +0100

    Queries and Commands Handlers Implementations

---
+++ CqrsMediatRPattern.API/Handlers/GetStudentByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using CqrsMediatRPattern.API.Models;
+using CqrsMediatRPattern.API.Queries;
+using CqrsMediatRPattern.API.Repositories.Interfaces;
+using MediatR;
+
+namespace CqrsMediatRPattern.API.Handlers
+{
+    public class GetStudentByIdHandler : IRequestHandler<GetStudentByIdQuery, Student>
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public GetStudentByIdHandler(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public async Task<Student> Handle(GetStudentByIdQuery query, CancellationToken cancellationToken)
+        {
+            return await _studentRepository.GetStudentByIdAsync(query.Id);
+        }
+    }
+}
---
+++ CqrsMediatRPattern.API/Handlers/UpdateStudentHandler.cs
@@ -0,0 +1,34 @@
+﻿using CqrsMediatRPattern.API.Commands;
+using CqrsMediatRPattern.API.Models;
+using CqrsMediatRPattern.API.Repositories.Interfaces;
+using MediatR;
+
+namespace CqrsMediatRPattern.API.Handlers
+{
+    public class UpdateStudentHandler : IRequestHandler<UpdateStudentCommand, int>
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public UpdateStudentHandler(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+        public async Task<int> Handle(UpdateStudentCommand command, CancellationToken cancellationToken)
+        {
+            var student = await _studentRepository.GetStudentByIdAsync(command.Id);
+
+            if (student is null)
+            {
+                return default;
+            }
+
+            student.Name = command.Name;
+            student.Email = command.Email;
+            student.Address = command.Address;
+            student.Age = command.Age;
+
+            return await _studentRepository.UpdateStudentAsync(student);
+           
+        }
+    }
+}
