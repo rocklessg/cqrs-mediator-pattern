1f24562067fae391f676529426bc6ba01bd50abd
Abdulhafiz <adanmaigoro@fsdhgroup.com>
2023-07-15 17:30:36 +0100

    Implement Db Configuration

--- CqrsMediatRPattern.API/Program.cs
+++ CqrsMediatRPattern.API/Program.cs
@@ -1,6 +1,16 @@
+using CqrsMediatRPattern.API.Data;
+using CqrsMediatRPattern.API.Repositories.Implementations;
+using CqrsMediatRPattern.API.Repositories.Interfaces;
+using MediatR;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
